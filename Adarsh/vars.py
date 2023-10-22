1493e3aa04f53b83e3c81c2d1113793d2629fa20
dktest2 <120356313+dktest2@users.noreply.github.com>
2023-10-22 20:46:05 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -13,7 +13,7 @@ class Var(object):
     API_HASH = str(getenv('API_HASH'))
     BOT_TOKEN = str(getenv('BOT_TOKEN'))
     API = str(getenv('API'))
-    name = str(getenv('name', 'filetolinkbot'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
